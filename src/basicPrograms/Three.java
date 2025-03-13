70099dba530232ad104e12483de314a12ff8a352
GangaSakthivel <gangasakthivel22@gmail.com>
2025-03-13 09:54:30 +0530

    Files were updated

---
+++ src/basicPrograms/Three.java
@@ -0,0 +1,41 @@
+package basicPrograms;
+
+public class Three {
+    public static void main(String[] args) {
+        //ternary operator ?:
+//        int num = 7;
+//        String result = (num % 2 == 0) ? "Even" : "Odd";
+//        System.out.println(result);
+//    }
+
+//        int age = 18;
+//        String result = (age >= 18) ? "Eligible to vote" : "Not eligible to vote";
+//        System.out.println(result);
+//
+//        Concise – Reduces if-else statements into a single line.
+//✅ Readable – Makes simple conditions easier to understand.
+//✅ Faster Execution – Slightly more efficient than if-else because it's an expression.
+
+        //Unary operators
+
+        //Bitwise Complement (~)
+//        int a = 10; //formula--> ~x = -(x + 1)
+//        System.out.println(~a);
+
+
+       //Logical not
+//        boolean flag = true;
+//        System.out.println(!flag);
+
+        //increment operator
+
+        //post increment
+        //pre increment
+        int a = 5;
+        System.out.println(++a); //pre increment becomes 6
+        System.out.println(a++); //post increment becomes 6
+        System.out.println(a); // then becomes 7
+
+
+    }
+}
--- src/basicPrograms/q1.java
+++ src/basicPrograms/q1.java
@@ -8,17 +8,22 @@ public class q1 {
 //        int age = sc.nextInt();
 //        System.out.print("My name is " + name + "\n" + "My age is " + age);
 
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int d = a * b * c;
-        int e = a + b + c;
-        System.out.println(d/e);
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        int c = sc.nextInt();
+//        int d = a * b * c;
+//        int e = a + b + c;
+//        System.out.println(d / e);
 
 
-    }
+                int a = 5, b = 3;
+                System.out.println("a & b: " + (a & b));
+                System.out.println("a | b: " + (a | b));
+                System.out.println("a ^ b: " + (a ^ b));
+                System.out.println("~a: " + (~a));
+                System.out.println("a << 1: " + (a << 1));
+                System.out.println("a >> 1: " + (a >> 1));
+            }
+        }
 
-
-
-}
