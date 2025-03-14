df3cc75fa95d30d4569448769b3ffb3bbe031cf6
GangaSakthivel <gangasakthivel22@gmail.com>
2025-03-14 20:52:08 +0530

    files were added

---
+++ src/Loops.java
@@ -0,0 +1,20 @@
+public class Loops {
+    public static void main(String[] args) {
+//        int i = 50;
+//        while(i >= 40){
+//            System.out.println(i);
+//            i--;
+//        }
+
+
+        //equals() only works with object
+//        int a = 5;
+//        int b = 5;
+//        System.out.println(a == b);
+
+        String s1 = "Ganga";
+        String s2 = "Ganga";
+        String s3 = new String("Ganga");
+        
+    }
+}
--- src/basicPrograms/Statements.java
+++ src/basicPrograms/Statements.java
@@ -58,5 +58,11 @@ public class Statements {
 //        for(int i = 10; i >= 1; i-=1){
 //            System.out.println(i);
 //       }
+
+
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Ganga");
+        }
     }
 }
