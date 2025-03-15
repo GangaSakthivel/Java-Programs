5e7c6f9e660a71e9d4bd29559fc0d856dd2ec953
GangaSakthivel <gangasakthivel22@gmail.com>
2025-03-15 11:20:42 +0530

    Added

--- src/basicPrograms/OopsPractice.java
+++ src/basicPrograms/OopsPractice.java
@@ -1,4 +1,25 @@
 package basicPrograms;
 
+
+import java.util.Scanner;
+
 public class OopsPractice {
+
+    public static int add(int x, int y) {  // Different names, but same logic
+        return x + y;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numOne = sc.nextInt();
+        int numTwo = sc.nextInt();
+        int result = add(numOne, numTwo);
+        System.out.println(result);
+
+
+
+    }
+
+
 }
