9f037851f2598260da82d9de240b1305f5060a15
GangaSakthivel <gangasakthivel22@gmail.com>
2025-03-15 12:46:53 +0530

    files were added

--- src/basicPrograms/OopsPractice.java
+++ src/basicPrograms/OopsPractice.java
@@ -4,22 +4,37 @@ package basicPrograms;
 import java.util.Scanner;
 
 public class OopsPractice {
-
-    public static int add(int x, int y) {  // Different names, but same logic
-        return x + y;
+    public static void sum(){
+        int sum = 0;
+        for(int i = 1; i <= 10; i++){
+            sum+=i;
+        }
+        System.out.println(sum);
     }
 
+    public static void five(){
+        for(int i = 1; i <= 10; i++){
+            System.out.println("5x" + i + "=" + i* 5);
+        }
+    }
+
+    public static int factorial(int n){
+        int fact = 1;
+        for(int i = 1; i <= n; i++){
+            fact *= i; //fact = fact * i
+        }
+        return fact;
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int numOne = sc.nextInt();
-        int numTwo = sc.nextInt();
-        int result = add(numOne, numTwo);
-        System.out.println(result);
-
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+        System.out.println("Factorial of " + n + " is " + factorial(n));
+        //sum();
+        //five();
 
 
     }
-
 
 }
