820cca09260ed09d72f6a011f2f2fcba611b0bfd
GangaSakthivel <gangasakthivel22@gmail.com>
2025-03-16 20:43:47 +0530

    files wer added

---
+++ src/basicPrograms/Practice.java
@@ -0,0 +1,28 @@
+package basicPrograms;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Practice {
+    public static void main(String[] args) {
+//        int number = 10;
+//        double numberTwo = number;
+
+//        double number = 4.49;
+//        int result = (int) number;
+//        System.out.println(result);
+
+//        int maxScore = 500;
+//        int underScore = 455;
+//        float percentage = (float) underScore*maxScore/100f;
+//        System.out.println(percentage);
+
+        String name1 = "ganga";
+        String name2 = "Ganga";
+        System.out.println(name1.contains("an"));
+
+
+
+    }
+
+
+}
---
+++ src/basicPrograms/Workouts.java
@@ -0,0 +1,29 @@
+package basicPrograms;
+
+
+
+public class Workouts {
+//    static void myMethod(String name, int age){
+//        System.out.println("Fname" + name + " AGe" );
+//    }
+
+
+    public static int add(int numOne, int numbTwo){
+        return numOne + numbTwo;
+    }
+
+    public static double multiply(float numOne, float numbTwo){
+        return numOne * numbTwo;
+    }
+
+    public static void main(String[] args) {
+//        myMethod("Ganga", 23);
+
+//        System.out.println(add(20, 100));
+//        System.out.println(multiply(10, 2));
+
+
+    }
+
+
+}
