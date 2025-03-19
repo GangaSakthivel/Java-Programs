7a055123d3cb3342ca06850f01f5b046b19d978a
GangaSakthivel <gangasakthivel22@gmail.com>
2025-03-19 11:23:29 +0530

    code Updated

--- src/programs/PalindromeChecker.java
+++ src/programs/PalindromeChecker.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class PalindromeChecker {
 
-    public static void main(String[] args) {
+    public static void isPalindrome(){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the word: ");
         String input = sc.nextLine();
@@ -17,6 +17,65 @@ public class PalindromeChecker {
         {
             System.out.print("Not a palindrome.");
         }
+    }
+
+    public static void controlFlowWitch(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value: ");
+        int value = sc.nextInt();
+        switch (value){
+            case 1:
+                System.out.print("Its monday. ");
+                break;
+            case 2:
+                System.out.println("Its tuesday. ");
+                break;
+            case 3:
+                System.out.println("Its wednesday. ");
+                break;
+            case 4:
+                System.out.println("Thursday. ");
+                break;
+            case 5:
+                System.out.println("Friday.");
+                break;
+            default:
+                System.out.println("Enter a day.");
+        }
+    }
+
+
+    public static void ATMpinverification(){
+        Scanner scanner = new Scanner(System.in);
+        final int correctPin = 1234; // Predefined correct PIN
+        int attempts = 0;
+        int userPin;
+
+        do {
+            System.out.print("Enter your 4-digit PIN: ");
+            userPin = scanner.nextInt();
+            attempts++;
+
+            if (userPin == correctPin) {
+                System.out.println("Access Granted!");
+                break;
+            } else {
+                System.out.println("Incorrect PIN. Try again.");
+            }
+        } while (attempts < 3);
+
+        if (userPin != correctPin) {
+            System.out.println("Too many failed attempts. Account locked.");
+        }
+
+
+}
+
+    public static void main(String[] args) {
+        //isPalindrome();
+        //controlFlowWitch();
+        ATMpinverification();
+
 
     }
 }
