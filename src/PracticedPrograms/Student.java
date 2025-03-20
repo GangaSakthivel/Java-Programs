437d2f275b77a62e730a1bc024590049e6ce09ce
GangaSakthivel <gangasakthivel22@gmail.com>
2025-03-20 13:52:34 +0530

    Constructors

---
+++ src/PracticedPrograms/Student.java
@@ -0,0 +1,27 @@
+package PracticedPrograms;
+
+//java constructor
+
+public class Student {
+//    int marks; //default 0
+//    String name;// default null
+
+//    Student(){
+//        System.out.println("Hello there! ");
+//    }
+//
+//    Student(String n){
+//        name = n;
+//        System.out.println("The name is " + n);
+//
+//    }
+
+    public static void main(String[] args) {
+//        Student s1 = new Student();
+//        Student s2 = new Student("Ganga");
+
+
+
+
+    }
+}
