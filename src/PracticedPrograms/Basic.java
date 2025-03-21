3f34dd131398493974a5e332ee429161b2b5fbd5
GangaSakthivel <gangasakthivel22@gmail.com>
2025-03-21 17:55:20 +0530

    Coding

--- src/PracticedPrograms/Basic.java
+++ src/PracticedPrograms/Basic.java
@@ -4,23 +4,52 @@ import java.util.Scanner;
 
 public class Basic {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number: ");
-        int n = sc.nextInt();
-        for(int i = 1; i <= n; i++){
-            if(i == 3){
-            break;
+    public static void program1(){ Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the amount: ");
+        int income = sc.nextInt();
+        if(income > 7000){
+            System.out.println("Eligible for Scholarship");
+        }else
+        {
+            System.out.println("Not eligible. ");
         }
-            System.out.print("Enter the number: ");
-            int value = sc.nextInt();
-            System.out.println("You entered: " + value);
     }
+
+    public static void program2(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int number = sc.nextInt();
+        if(number % 3 == 0 && number % 5 == 0){
+            System.out.println("The number is divisible by 3 and 5");
+        }else {
+            System.out.println("The number is divisible by 3 and 5");
+        }
     }
+
+    public static void program3(){
+        int num = -7;
+        String result = (num > 0) ? "Positive" : (num < 0) ? "Zero" : "Negative";
+        System.out.println(result);
+    }
+
+    public static void program4(){
+        {
+         for(int i = 1; i <= 5; i++){
+             System.out.println("Ganga");
+         }
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        //program1();
+        //program2();
+        program3();
 }
 
 
-
+}
 
 
 
--- src/PracticedPrograms/JavaArray.java
+++ src/PracticedPrograms/JavaArray.java
@@ -4,7 +4,28 @@ import java.util.Scanner;
 
 public class JavaArray {
     public static void main(String[] args) {
-//
+//        String[] cars = new String[8];
+//        cars[0] = "Toyota";
+//        cars[1] = "Honda";
+//        cars[2] = "Ford";
+//        cars[3] = "Chevrolet";
+//        cars[4] = "BMW";
+//        cars[5] = "Mercedes";
+//        cars[6] = "Audi";
+//        cars[7] = "Nissan";
+//        System.out.println(cars.length);
+//        System.out.println(cars[0]);
+//        System.out.println(cars[5]);
+
+        //syntax
+        //datatype[] variable (reference) = new(keyword) datatype[size];
+        int[] rollNo = new int[10];
+        int[] rollNoTwo = {10, 5, 3, 4, 2};
+        int[] names; //declaration of array //here names varibale is getting defined in stack
+        names = new int[10]; //initialization
+        // this is where actual memory is created in the heap memory
+
+
 
 
     }
